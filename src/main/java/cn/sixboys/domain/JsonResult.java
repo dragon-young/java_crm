cda446f9e9882b9d3ea6750864f81bf13c8676e0
fuqiangwu <2287647323@qq.com>
2021-05-08 21:28:36 +0800

    环境搭建“ git commit -m 环境搭建 git commit -m 环境搭建“

---
+++ src/main/java/cn/sixboys/domain/JsonResult.java
@@ -0,0 +1,46 @@
+package cn.sixboys.domain;
+
+public class JsonResult {
+
+        private boolean success;
+        private String msg;
+        private Object data;
+
+        public JsonResult() {
+        }
+
+        public JsonResult(boolean success, String msg) {
+            this.success = success;
+            this.msg = msg;
+        }
+
+        public JsonResult(boolean success, String msg, Object data) {
+            this.success = success;
+            this.msg = msg;
+            this.data = data;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+    }
